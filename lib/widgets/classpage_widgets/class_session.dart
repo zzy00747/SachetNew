0e939ea04ad6229e3bf5204806a3e45223f8238d
zzy00747 <1285445576@qq.com>
2025-06-26 17:18:06 +0800

    fix(ui): 约束上课时间在一行内显示 (#1)

--- lib/widgets/classpage_widgets/class_session.dart
+++ lib/widgets/classpage_widgets/class_session.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sachet/provider/course_card_settings_provider.dart';
 import 'package:provider/provider.dart';
 
-// TODO: 为更多不同字体大小设计更好的展示方法
 class ClassSession extends StatelessWidget {
   /// 最左列显示课程节次、开始时间、结束时间的 Column 里的一个 Container
   const ClassSession({
@@ -22,31 +21,26 @@ class ClassSession extends StatelessWidget {
     return Container(
       height: cardHeight,
       color: Theme.of(context).colorScheme.surface,
-      child: SingleChildScrollView(
-        // scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 3.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  session,
-                  style: const TextStyle(
-                      fontSize: 13, fontWeight: FontWeight.bold),
-                  // overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  startTime,
-                  style: const TextStyle(fontSize: 13),
-                  // overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  endTime,
-                  style: const TextStyle(fontSize: 13),
-                  // overflow: TextOverflow.ellipsis,
-                ),
-              ]),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 3.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    session,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  FittedBox(
+                    child: Text(startTime),
+                  ),
+                  FittedBox(
+                    child: Text(endTime),
+                  ),
+                ]),
+          ),
         ),
       ),
     );
