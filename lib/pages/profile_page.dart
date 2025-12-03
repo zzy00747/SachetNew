f3252b12d9afd3751a661681b591cdbbdd05dad6
zzy00747 <1285445576@qq.com>
2025-12-03 13:56:20 +0800

    feat(ProfilePage): 移除旧教务系统账号信息显示

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,10 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:sachet/models/jwxt_type.dart';
 import 'package:sachet/pages/about_page.dart';
 import 'package:sachet/pages/settings_page.dart';
-import 'package:sachet/pages/utilspages/qiangzhi_jwxt_login_page.dart';
 import 'package:sachet/pages/utilspages/zhengfang_jwxt_login_page.dart';
 import 'package:sachet/providers/screen_nav_provider.dart';
-import 'package:sachet/providers/qiangzhi_user_provider.dart';
 import 'package:sachet/providers/zhengfang_user_provider.dart';
 import 'package:sachet/utils/utils_funtions.dart';
 import 'package:sachet/widgets/settingspage_widgets/settings_section_title.dart';
@@ -33,43 +31,6 @@ class _ProfilePageState extends State<ProfilePage> {
         appBar: AppBar(title: Text('我')),
         body: ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: SettingsSectionTitle(title: '旧教务系统'),
-            ),
-            // 强智教务系统
-            Selector<QiangZhiUserProvider, ({String name, String id})>(
-              selector: (_, qiangzhiUserProvider) => (
-                name: qiangzhiUserProvider.user.name ?? '未登录',
-                id: qiangzhiUserProvider.user.studentID ?? '点击登录'
-              ),
-              builder: (_, data, __) {
-                return ListTile(
-                    leading: Align(
-                      widthFactor: 1,
-                      alignment: Alignment.centerLeft,
-                      child: Icon(Icons.account_circle),
-                    ),
-                    title: Text(data.name),
-                    subtitle: Text(data.id),
-                    trailing: IconButton(
-                      onPressed: () async {
-                        await showLogoutDialog(context, JwxtType.qiangzhi);
-                      },
-                      tooltip: '退出登录',
-                      icon: Icon(Icons.logout_outlined),
-                    ),
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return const QiangZhiJwxtLoginPage();
-                          },
-                        ),
-                      );
-                    });
-              },
-            ),
             Padding(
               padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
               child: SettingsSectionTitle(title: '新教务系统'),
