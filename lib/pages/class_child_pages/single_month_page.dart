73a55d72d9d699d58feb5586538d80f615675e7d
zzy00747 <1285445576@qq.com>
2026-04-07 21:46:12 +0800

    feat(month_view): 如果某一周完全没有晚课, 则不显示晚课的空占位

--- lib/pages/class_child_pages/single_month_page.dart
+++ lib/pages/class_child_pages/single_month_page.dart
@@ -30,6 +30,17 @@ class SingleMonthPage extends StatefulWidget {
 class _SingleMonthPageState extends State<SingleMonthPage> {
   List<DateTime> _calendarDays = [];
 
+  /// 晚上的课（9,10,11 节）的 ItemIndex 列表
+  /// item:
+  ///
+  ///       Mon Tue Wed Thu Fri Sat Sun
+  /// 12      0   5  10  15  20  25  30
+  /// 34      1   6  11  16  21  26  31
+  /// 56      2   7  12  17  22  27  32
+  /// 78      3   8  13  18  23  28  33
+  /// 91011   4   9  14  19  24  29  34
+  static const List _nightCourseItemIndexes = [4, 9, 14, 19, 24, 29, 34];
+
   /// 构建一个月完整的日历日期列表（含上月/下月补全）
   void _buildCalendarDays() {
     final monthDate = widget.monthDate;
@@ -163,7 +174,7 @@ class _SingleMonthPageState extends State<SingleMonthPage> {
         child: Column(
           children: [
             Container(
-              padding: EdgeInsets.all(4.0),
+              padding: EdgeInsets.all(8.0),
               decoration: _isToday
                   ? BoxDecoration(
                       color: colorScheme.primary,
@@ -174,6 +185,7 @@ class _SingleMonthPageState extends State<SingleMonthPage> {
                 "${date.day}",
                 style: TextStyle(
                   fontSize: 12.5,
+                  height: 1.0,
                   color: !isInCurrentMonth
                       ? colorScheme.outline
                       : _isToday
@@ -192,6 +204,29 @@ class _SingleMonthPageState extends State<SingleMonthPage> {
                       .map((data) => CourseSchedule.fromJson(data))
                       .toList();
                   final cardHeight = 10.0;
+
+                  // 如果这一周完全没有晚课，不显示这一周所有的晚课的空白占位
+                  // 其他课，即使没有课，也会有占位的空白，以保证剩下的课在正确的位置上
+                  // 但如果是晚课，可以不显示，不会对课程的排列造成影响
+                  // 如果整周都没有晚课，则可以不显示这一周所有晚课的空白占位，能够节省月视图下稀缺的空间
+
+                  /// 这节课是否是晚课（显示在 **这个位置上** 的课是否是晚课，不一定有课）
+                  final isNightCourse = _nightCourseItemIndexes.contains(item);
+
+                  // 如果这节课是晚课，判断这一周是否每天晚上都没有课
+                  if (isNightCourse) {
+                    /// 这一周是否每天晚上都没有课
+                    final isWeekHasNoNightCourse = _thisWeekHasNoNightCourses(
+                      weekCount: weekCount,
+                      courseScheduleItemsList: widget.courseScheduleItemsList!,
+                      nightCourseItemIndexs: _nightCourseItemIndexes,
+                    );
+
+                    // 如果这一周没有一节晚课，则不显示 9,10,11 节课的空占位
+                    if (isWeekHasNoNightCourse) {
+                      return SizedBox();
+                    }
+                  }
                   return SizedBox(
                     height: (cardHeight) * 2,
                     child: OverflowBox(
@@ -214,4 +249,38 @@ class _SingleMonthPageState extends State<SingleMonthPage> {
       ),
     );
   }
+}
+
+/// 这一周是否每天晚上都没有课（晚课：9,10,11 节）
+///
+/// true: 这一周完全没有晚课
+///
+/// false: 这一周至少有一天有晚课
+bool _thisWeekHasNoNightCourses({
+  required int weekCount,
+  required List courseScheduleItemsList,
+  required List nightCourseItemIndexs,
+}) {
+  for (final nightCourseItemIndex in nightCourseItemIndexs) {
+    final List courses = courseScheduleItemsList[nightCourseItemIndex];
+    List<CourseSchedule> courseScheduleItems =
+        courses.map((data) => CourseSchedule.fromJson(data)).toList();
+    if (courseScheduleItems.isEmpty) {
+      continue;
+    }
+    int index = courseScheduleItems
+        .indexWhere((e) => e.weeks!.contains(weekCount) == true);
+    if (index == -1) {
+      continue;
+    } else {
+      CourseSchedule courseSchedule = courseScheduleItems[index];
+      if (courseSchedule.title == null || courseSchedule.title == '') {
+        continue;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
