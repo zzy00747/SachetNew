348615d7bcb6a8fc134e22e8dd2ddca3113c21a3
zzy00747 <1285445576@qq.com>
2026-04-28 17:20:22 +0800

    fix(month_view): 修复切换到月视图时可能跳转到非当前月份的问题

--- lib/pages/class_child_pages/month_view.dart
+++ lib/pages/class_child_pages/month_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sachet/constants/app_constants.dart';
 import 'package:sachet/models/course_schedule.dart';
 import 'package:sachet/pages/class_child_pages/single_month_page.dart';
 import 'package:sachet/providers/class_page_provider.dart';
@@ -39,9 +40,11 @@ class _MonthViewState extends State<MonthView> {
           context.read<ClassPageProvider>().currentWeekCount;
 
       final currentMonth = getDateFromWeekCountAndWeekday(
-        semesterStartDate: DateTime.parse(SettingsProvider.semesterStartDate),
+        semesterStartDate:
+            DateTime.tryParse(SettingsProvider.semesterStartDate) ??
+                constSemesterStartDate,
         weekCount: currentWeekCount,
-        weekday: 7,
+        weekday: DateTime.now().weekday,
       ).month;
       context.read<ClassPageProvider>().jumpToMonth(month: currentMonth);
     });
